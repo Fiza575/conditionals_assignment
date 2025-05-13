c1a4c0d171c7b5f9a8a15227a29cd7b6c074a0af
Fiza Arzoo Khalid <shaizakanwal025@gmail.com>
2025-05-13 18:56:11 +0500

    Add files via upload

---
+++ Conditionals_assignment.py
@@ -0,0 +1,57 @@
+# Task no 1 (Check age)
+
+age = 26
+if age >= 18:
+    print("You are an adult")
+
+# Task no 2 (Age check with else)
+
+age = 15
+if age >= 18:
+    print("You are an adult")
+else:
+    print("You are not an adult")
+
+# Task no 3 (Grading System)
+
+marks = 50
+if marks >= 90:
+    print("Grade:A")
+elif marks >= 70:
+    print("Grade:B")
+elif marks >= 50:
+    print("Grade:C")
+else :
+    print("Fail")
+
+# Task no 4 (Weather Advice)
+
+weather = "cloudy"
+if weather == "rainy":
+    print("Take an umbrella")
+elif weather == "sunny":
+    print("Wear sunglasses")
+elif weather == "cloudy":
+    print("It might rain later")
+
+# Task no 5 (Quiz feeling hungry)
+
+hungry = True
+print("Go eat something")
+hungry = False
+print("Have a snack later")
+
+# Task no 6 (Mini chatbot)
+
+mood =input("How are you feeling today?").lower()
+if mood == "happy":
+    print("That's great to hear!")
+if mood == "sad":
+    print("I hope your day gets better.")
+if mood == "bored":
+    print("Try reading a book or going for walk.")
+else:
+    print ("Thanks for sharing your mood!")
+   
+
+
